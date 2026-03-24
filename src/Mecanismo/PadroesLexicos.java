e8701ffb62fb9f553e0e973cf07c92c2d42069d5
Diego <diegomilitaosouza@gmail.com>
2026-03-23 20:50:55 -0400

    Padrao Lexico

--- src/Mecanismo/PadroesLexicos.java
+++ src/Mecanismo/PadroesLexicos.java
@@ -1,12 +1,12 @@
 package Mecanismo;
 
 public class PadroesLexicos {
-    public static String COMMENT = "\\/\\/.* | \\(\\*(?:. | \\S)*\\*\\)";
-    public static String NUMBER = "";
-    public static String LITERAL = "";
-    public static String WORDS = "";
-    public static String CHARACTER = "";
-    public static String IDENTIFIER = "";
+    public static String COMMENT = "\\/\\/.*|\\(\\*(?:.|\\S)*\\*\\)";
+    public static String NUMBER = "(?<!\\w)(?:-?\\d+(?:\\.\\d+)?)(?!\\w)";
+    public static String LITERAL = "'(?:[^']|'')*'";
+    public static String WORDS = "\\w+";
+    public static String CHARACTER = "(?::=|>=|<=|<>|>|<|=|\\+|\\-|\\*|\\/|[:;,.()\\[\\]{}])";
+    public static String IDENTIFIER = "^[A-Za-z][A-Za-z0-9_]*$";
 
     
 
